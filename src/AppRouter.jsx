f067627ee530c5375e65d610742ea7b7f941ef57
Juan Gomez <gomex086@gmail.com>
2023-08-20 20:48:16 -0500

    Pokedex
    Signed-off-by: Juan Gomez <gomex086@gmail.com>

---
+++ src/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { React } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Navigation } from './components/Navigation';
+import { HomePage, PokemonPage, SearchPage } from './pages';
+
+export const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Navigation/>}>
+                <Route index element={<HomePage/>} />
+                <Route path='pokemon/:id' element={<PokemonPage/>} />
+                <Route path='search' element={<SearchPage/>} />
+            </Route>
+                <Route path='*' element={<Navigate to='/'/>}  />
+        </Routes>
+    );
+};
